caab65b081a241fd90772c4434444692f0ba6b68
Paul Couthouis <paulcouthouis@gmail.com>
2020-12-30 17:21:37 +0900

    feat: init mobile-header and main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileHeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
