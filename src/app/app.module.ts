da706016f8fb3e4695e25a5df10b04695d92b128
Paul Couthouis <paulcouthouis@gmail.com>
2020-12-31 16:25:40 +0900

    feat: init subcomponent of main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
 import { MainComponent } from './main/main.component';
+import { AboutMeComponent } from './main/about-me/about-me.component';
+import { ResumeComponent } from './main/resume/resume.component';
+import { ContactComponent } from './main/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MobileHeaderComponent,
-    MainComponent
+    MainComponent,
+    AboutMeComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/resume/resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pol-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
