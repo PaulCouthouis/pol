171f968dd4d5f006848e1949b220037728a86286
Paul Couthouis <paulcouthouis@gmail.com>
2020-12-31 17:28:35 +0900

    feat : hide element because change strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,16 @@ import { AppComponent } from './app.component';
 import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
 import { MainComponent } from './main/main.component';
 import { AboutMeComponent } from './main/about-me/about-me.component';
-import { ResumeComponent } from './main/resume/resume.component';
-import { ContactComponent } from './main/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MobileHeaderComponent,
     MainComponent,
-    AboutMeComponent,
-    ResumeComponent,
-    ContactComponent
+    AboutMeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
