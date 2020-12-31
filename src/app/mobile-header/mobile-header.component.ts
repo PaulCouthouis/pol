2910b9eeea97726375d21685faccc91d5095a589
Paul Couthouis <paulcouthouis@gmail.com>
2020-12-31 16:15:11 +0900

    feat: design mobile-header

--- src/app/mobile-header/mobile-header.component.ts
+++ src/app/mobile-header/mobile-header.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pol-mobile-header',
   templateUrl: './mobile-header.component.html',
   styleUrls: ['./mobile-header.component.scss']
 })
-export class MobileHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MobileHeaderComponent {
+  name = 'Paul Couthouis';
+  photo = 'assets/images/photo.jpg';
 }
